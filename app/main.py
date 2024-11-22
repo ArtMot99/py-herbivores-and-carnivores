1fcde67467d483be6242e9c9ef3ee97919baffa5
Artem Motrunich <motrunich2013@gmail.com>
2024-11-22 17:49:11 +0100

    Solution_v2

--- app/main.py
+++ app/main.py
@@ -8,11 +8,11 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return "{"\
-               f"Name: {self.name}, " \
-               f"Health: {self.health}," \
-               f" Hidden: {self.hidden}"\
-               "}"
+        return (
+            f"{{Name: {self.name},"
+            f" Health: {self.health},"
+            f" Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
@@ -25,8 +25,7 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
 
-    @staticmethod
-    def bite(animal: Herbivore) -> None:
+    def bite(self, animal: Herbivore) -> None:
         if not animal.hidden and isinstance(animal, Herbivore):
             animal.health -= 50
 
